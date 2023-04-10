d5142e370bf75385a3de706f4072088d5f5bb32d
Michiel Westerbeek <happylinks@gmail.com>
2023-04-10 17:19:41 +0200

    updated to use webcodecs

--- codecs-video-vp9.js
+++ codecs-video-vp9.js
@@ -1,9 +1,11 @@
 function getAllVP9Codecs() {
-	// TODO generate all possible profiles
-	// vp09.<profile>.<level>.<bitDepth>[.<chromaSubsampling>.<colourPrimaries>.<transferCharacteristics>.<matrixCoefficients>.<videoFullRangeFlag>]
+  // TODO generate all possible profiles
+  // vp09.<profile>.<level>.<bitDepth>[.<chromaSubsampling>.<colourPrimaries>.<transferCharacteristics>.<matrixCoefficients>.<videoFullRangeFlag>]
 
-	return [
-		{ codec: "vp09.00.51.08.01.01.01.01.00", description: "VP9 YouTube 8K" },
-		{ codec: "vp09.00.50.08", description: "VP9, Profile 0, Level 5, 8-bit"}
-	];
+  return [
+    {codec: "vp8", description: "VP8"},
+    {codec: "vp9", description: "VP9"},
+    {codec: "vp09.00.51.08.01.01.01.01.00", description: "VP9 YouTube 8K"},
+    {codec: "vp09.00.50.08", description: "VP9, Profile 0, Level 5, 8-bit"}
+  ];
 }
--- index.js
+++ index.js
@@ -1,84 +1,106 @@
-function addMimeChecks(table,mime,desc) { 
-	var video = document.createElement("video");
-	var tr;
-	var video_result;
-	var mediasource_result;
-	video_result = video.canPlayType(mime);
-	try { 
-		mediasource_result = MediaSource.isTypeSupported(mime); 
-	} catch (e) { 
-		mediasource_result = e.message; 
-	}
-	tr = document.createElement("tr");
-	tr.innerHTML = "<td id='"+mime+"'>"+desc+"</td><td><a href='#"+mime+"'>"+mime+"</a></td><td class='"+(video_result === ""? "fail" : (video_result === "maybe" ? "maybe": "ok"))+"'>"+(video_result.length > 0 ? video_result : "no")+"</td><td class='"+(mediasource_result === true ? "ok" : "fail")+"'>"+mediasource_result+"</td>";
-	table.appendChild(tr);
+async function addMimeChecks(table, mime, desc, audio) {
+  var tr;
+
+  var supportHardware = {
+    supported: false
+  };
+  try {
+    supportHardware = await (audio ? AudioEncoder : VideoEncoder).isConfigSupported({
+      codec: mime,
+      hardwareAcceleration: "prefer-hardware",
+      width: audio ? null : 1920,
+      height: audio ? null : 1080,
+      bitrate: audio ? 128_000 : 2_000_000,
+      numberOfChannels: audio ? 1 : null,
+      sampleRate: audio ? 44100 : null
+    });
+  } catch (e) {
+    supportHardware = {
+      supported: false
+    }
+  }
+
+  var supportSoftware = {
+    supported: false
+  };
+  try {
+    supportSoftware = await (audio ? AudioEncoder : VideoEncoder).isConfigSupported({
+      codec: mime,
+      hardwareAcceleration: "prefer-software",
+      width: audio ? null : 1920,
+      height: audio ? null : 1080,
+      bitrate: audio ? 128_000 : 2_000_000,
+      numberOfChannels: audio ? 1 : null,
+      sampleRate: audio ? 44100 : null
+    });
+  } catch (e) {
+    supportSoftware = {
+      supported: false
+    }
+  }
+
+  tr = document.createElement("tr");
+  tr.innerHTML = "<td id='" + mime + "'>" + desc + "</td><td><a href='#" + mime + "'>" + mime + "</a></td><td class='" + (supportHardware.supported ? "ok" : "fail") + "'>" + (supportHardware.supported ? "yes" : "no") + "</td><td class='" + (supportSoftware.supported ? "ok" : "fail") + "'>" + (supportSoftware.supported ? "yes" : "no") + "</td>";
+  table.appendChild(tr);
 }
 
 function createTableHeader(div) {
-	var t = document.createElement("table");
-	div.appendChild(t);
-	var content = "";
-	content += "<tr>";
-	content += "<th>Description</th>";
-	content += "<th>MIME</th>";
-	content += "<th><code>HTMLMediaElement.canPlayType</code></th>";
-	content += "<th><code>MediaSource.isTypeSupported</code></th>";
-	content += "</tr>";
-	t.innerHTML = content;
-	return t;
+  var t = document.createElement("table");
+  div.appendChild(t);
+  var content = "";
+  content += "<tr>";
+  content += "<th>Description</th>";
+  content += "<th>Codec</th>";
+  content += "<th><code>prefer-hardware</code></th>";
+  content += "<th><code>prefer-software</code></th>";
+  content += "</tr>";
+  t.innerHTML = content;
+  return t;
 }
 
 function addParagraph(div, id, msg) {
-	var p = document.createElement("p");
-	p.setAttribute("id", id);
-	p.innerHTML = msg;
-	div.appendChild(p);
+  var p = document.createElement("p");
+  p.setAttribute("id", id);
+  p.innerHTML = msg;
+  div.appendChild(p);
 }
 
-window.onload = function() {
-	var results = document.getElementById("results");
-	var table, i;
+window.onload = async function () {
+  var results = document.getElementById("results");
+  var table, i;
 
-	addParagraph(results, "general_mimes", "Checking support for MIME types registered in <a href='https://tools.ietf.org/html/rfc6381'>RFC6381</a>");
-	table = createTableHeader(results);
-	for(i in MIMES) {
-		addMimeChecks(table,MIMES[i], "");
-	}
-	addParagraph(results, "general_codecs", "Checking support for codecs registered in <a href='http://mp4ra.org/codecs.html'>MP4RA</a>");
-	table = createTableHeader(results);
-	for(i in CODECS) {
-		var codec = CODECS[i].codec;
-		addMimeChecks(table,'video/mp4; codecs="'+codec+'"', CODECS[i].description);
-	}
-	addParagraph(results, "audio_codecs", "Checking support for audio codecs with parameters");
-	table = createTableHeader(results);
-	for(i in AUDIO_CODECS) {
-		var codec = AUDIO_CODECS[i].codec;
-		addMimeChecks(table, 'audio/mp4; codecs="'+codec+'"', AUDIO_CODECS[i].description);
-	}
-	addParagraph(results, "avc_codecs", "Checking support for AVC codecs with parameters");
-	table = createTableHeader(results);
-	addChecks(getAllAVCCodecs, addMimeChecks, table);
+  addParagraph(results, "audio_codecs", "Checking support for audio codecs with parameters");
+  table = createTableHeader(results);
+  for (i in AUDIO_CODECS) {
+    var codec = AUDIO_CODECS[i].codec;
+    await addMimeChecks(table, codec, AUDIO_CODECS[i].description, true);
+  }
+  addParagraph(results, "avc_codecs", "Checking support for AVC codecs with parameters");
+  table = createTableHeader(results);
+  addChecks(getAllAVCCodecs, addMimeChecks, table);
 
-	addParagraph(results, "av1_codecs", "Checking support for AV1 codecs with parameters");
-	table = createTableHeader(results);
-	addChecks(getAllAV1Codecs, addMimeChecks, table);
+  addParagraph(results, "av1_codecs", "Checking support for AV1 codecs with parameters");
+  table = createTableHeader(results);
+  addChecks(getAllAV1Codecs, addMimeChecks, table);
 
-	addParagraph(results, "other_video_codecs", "Checking support for other codecs such as VP9, HEVC");
-	table = createTableHeader(results);
-	addChecks(getAllVP9Codecs, addMimeChecks, table);
-	addChecks(getAllHEVCCodecs, addMimeChecks, table);
+  addParagraph(results, "other_video_codecs", "Checking support for other codecs such as VP9, HEVC");
+  table = createTableHeader(results);
+  addChecks(getAllVP9Codecs, addMimeChecks, table);
+  addChecks(getAllHEVCCodecs, addMimeChecks, table);
 };
 
-function addOwnTest(id, v) {
-	var results = document.getElementById(id);
-	results.innerHTML='';
-	table = createTableHeader(results);
-	addMimeChecks(table, 'video/mp4; codecs="'+v+'"', "");
+async function addOwnTest(id, v) {
+  var results = document.getElementById(id);
+  results.innerHTML = '';
+  table = createTableHeader(results);
+  await addMimeChecks(table, v, "");
 }
 
-function addChecks(getAllCodecs, add,table)
-{
-	getAllCodecs().forEach(e => add(table, 'video/mp4; codecs="'+e.codec+'"', e.description));
+async function addChecks(getAllCodecs, add, table) {
+  let codecs = getAllCodecs();
+  for (i in codecs) {
+    let e = codecs[i];
+    await add(table, e.codec, e.description);
+  }
 }
 
